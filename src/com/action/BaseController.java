833f14cef89d3e3e7389f73fc735257c96a27765
xuxiaoxie <xuzhijian2012@163.com>
2014-08-24 11:32:52 +0800

    wechat项目初始化
    最原始页面

---
+++ src/com/action/BaseController.java
@@ -0,0 +1,10 @@
+package com.action;
+
+import com.common.message.MsgBody;
+
+
+public class BaseController {
+	
+	MsgBody msgBody=new MsgBody();
+
+}
---
+++ src/com/common/exception/BusinessException.java
@@ -0,0 +1,5 @@
+package com.common.exception;
+
+public class BusinessException extends Exception{
+
+}
